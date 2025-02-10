de1d41a9663729133d5ab7a0ff20799065883e5c
Joan Spaeth <xensevi@gmail.com>
2025-02-09 21:18:28 -0800

    Update utils.cc

--- utils.cc
+++ utils.cc
@@ -18,6 +18,22 @@ void vectorPrint(float* u , int rows){
 
 }
 
+void vectorNorm(float* u, int rowsU, float* z){
+  float sum = 0;
+  float scalar = 0;
+  
+  for (int i = 0; i <= rowsU; i++)
+    sum += pow(u[i]);
+  scalar = sqrt(sum);
+  for (int j = 0; j<=rowsU; j++)
+    z[j] = (u[j] / scalar);
+    
+}
+
+void vectorDotProduct(float* u, float* v, int rowsU, int rowsV, float* z){ 
+  
+}
+
 void vectorScale(float* u, int rows, float alpha , float* v){
   for(int i=0; i<rows; i++){
     v[i]=alpha*u[i];
@@ -29,3 +45,4 @@ void vectorSubtract(float* u, int rowsU, float* v, int rowsV, float* z){
   for(int i = 0; i<rowsU; i++)
     z[i] = u[i] - v[i];
 }
+
